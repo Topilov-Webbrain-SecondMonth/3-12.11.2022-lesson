f9cded50b2ec3b1f9b22b1ef0aa18679f3fda2ce
topilovasilbek <asilbektback@gmail.com>
2022-11-12 22:07:09 +0500

    third lesson some practise in the evening

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// Third lesson some practise
+
+// Operations
+// let a = 1;
+// let b = "1";
+// console.log(a + 1); console.log(a - 1); console.log(a * 1); console.log(a / 1);
+// console.log(a + b) // 11
+// console.log(a + +b) // 2
+// console.log(a - b) // 0
+// console.log(a * b) // 1
+// console.log(a / b) // 1
+
+
+// let number = 10;
+// console.log(number++); // 10  ----  Avval console ga chiqazib, keyin 1 ni qoshadi
+// console.log(number); // 11
+// console.log(++number); // 12  ---- Avval number ga 1 ni qo`shib keyin console ga chiqazadi
+
+
+// console.log(null == undefined) // Faqat value ni tekshiradi
+// console.log(null === undefined) // Value ni va Data type ni tekshiradi
+
+// console.log(1 == true) // true   -----If number is !0, then it is true
+// console.log(1 === true) // false
+// console.log(Boolean(0)) // false
+
+// console.log(2 > 100) // false
+// console.log(2 < 100) // true
+// console.log(2 == 100) // false
+// console.log(2 == 2) // true
+// console.log(2 === 2) // true
